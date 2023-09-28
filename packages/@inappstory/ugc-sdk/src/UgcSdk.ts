7ac161b43a69796b0057f7ecb00ce921c9012f0e
a.strebizh <artem.strebizh@inappstory.com>
2023-09-28 15:05:22 +0300

    build(🚀ugc-sdk): 1.0.4
    feat(UgcSdk): add static get sdkVersionName and static get sdkVersionCode for UgcSdk class

--- packages/@inappstory/ugc-sdk/src/UgcSdk.ts
+++ packages/@inappstory/ugc-sdk/src/UgcSdk.ts
@@ -23,4 +23,14 @@ export class UgcSdk {
         UgcSdk._viewModel = viewModel;
     }
 
+    public static get sdkVersionName(): string {
+        const sdkVersionName = process.env.SDK_VERSION;
+        return sdkVersionName || "0.0.0";
+    }
+
+    public static get sdkVersionCode(): number {
+        const sdkVersionCode = process.env.SDK_VERSION_CODE;
+        return sdkVersionCode ? parseInt(String(sdkVersionCode)) : 0;
+    }
+
 }
